61922c9da88ab78bf247b3f52840e50d03eafbe5
Axel-Cleris Gailloty <mr.clerisaxel@hotmail.fr>
2023-04-03 18:13:24 +0200

    Updating ONNX model

--- model.py
+++ model.py
@@ -3,13 +3,13 @@ import torch
 best_model = torch.load("bestmodel.pt")
 
 # NCHW (batch size, number of channels, height, width).
-x = torch.randn(15, 3, 224, 224, requires_grad=True).cuda()
+x = torch.randn(1, 3, 224, 224, requires_grad=True).cuda()
 
 torch.onnx.export(best_model,
                  x,
                  "bestmodel.onnx",
                  verbose=False,
-                 input_names=["actual_input"],
-                 output_names=["output"],
+                 input_names=["ModelInput"],
+                 output_names=["Output"],
                  export_params=True,
                  )
